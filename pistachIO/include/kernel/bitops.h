a6c52276fee60ded9dac0f5d4a9ae16985a9a7ab
yihan <yangyihan1998@gmail.com>
2024-03-22 02:34:19 +0000

    test

--- pistachIO/include/kernel/bitops.h
+++ pistachIO/include/kernel/bitops.h
@@ -33,19 +33,19 @@ static inline void set_bit(int nr, volatile void * addr) {
 #if defined(__x86_64__) || defined(__i386__)
     asm volatile("btsl %1, %0" : "+m" (*(uint32_t *)addr) : "Ir" (nr));
 #elif defined(__arm__) || defined(__aarch64__)
-	uint32_t * work = (uint32_t *)addr;
-    uint32_t mask = 1U << (nr & 31); // Calculate mask for bit to set
+	volatile uint32_t *vaddr = (uint32_t *)addr;
+    uint32_t mask = 1U << nr;
     uint32_t old, tmp;
 
-    asm volatile (
-		"1:     ldrex %0, [%2]\n\t"
-		"       orr %0, %0, %3\n\t"
-		"       strex %1, %0, [%2]\n\t"
-		"       teq %1, #0\n\t"
-		"       bne 1b"
-		: "=&r" (old), "=&r" (tmp), "+r" (work)
-		: "r" (mask)
-		: "cc", "memory"
+    __asm__ volatile (
+        "1:     ldrex   %0, [%2]        \n" // Load the value exclusively
+        "       orr     %0, %0, %3      \n" // Set the bit using ORR
+        "       strex   %1, %0, [%2]    \n" // Attempt to store the new value
+        "       teq     %1, #0          \n" // Test if the store was successful
+        "       bne     1b              "   // If not, retry
+        : "=&r" (old), "=&r" (tmp)
+        : "r" (vaddr), "r" (mask)
+        : "cc", "memory"
     );
 #else
 	pr_err("Unknown architecture!\n");
@@ -56,41 +56,23 @@ static inline void clear_bit(int nr, volatile void * addr) {
 #if defined(__x86_64__) || defined(__i386__)
     asm volatile("btrl %1, %0" : "+m" (*(uint32_t *)addr) : "Ir" (nr));
 #elif defined(__arm__) || defined(__aarch64__)
-    uint32_t *word = (uint32_t *)addr;
-    uint32_t mask = 1U << (nr & 31); // Calculate mask for bit to clear
+	volatile uint32_t *vaddr = (uint32_t *)addr;
+    uint32_t mask = 1U << nr;
     uint32_t old, tmp;
 
-    asm volatile (
-		"1:     ldrex %0, [%2]\n\t"
-		"       bic %0, %0, %3\n\t"
-		"       strex %1, %0, [%2]\n\t"
-		"       teq %1, #0\n\t"
-		"       bne 1b"
-		: "=&r" (old), "=&r" (tmp), "+r" (word)
-		: "r" (mask)
-		: "cc", "memory"
+    __asm__ volatile (
+        "1:     ldrex   %0, [%2]        \n" // Load the current value exclusively
+        "       bic     %0, %0, %3      \n" // Clear the bit using Bit Clear (BIC)
+        "       strex   %1, %0, [%2]    \n" // Attempt to store the new value
+        "       teq     %1, #0          \n" // Test if the store was successful
+        "       bne     1b              "   // If not, retry
+        : "=&r" (old), "=&r" (tmp)
+        : "r" (vaddr), "r" (mask)
+        : "cc", "memory"
     );
 #else
 	pr_err("Unknown architecture!\n");
 #endif
-}
-
-void atomic_increment(volatile int *ptr) {
-    int success, value;
-    do {
-        // Load the current value of *ptr exclusively
-        asm volatile ("ldrex %0, [%1]"
-                      : "=r" (value)
-                      : "r" (ptr));
-        // Increment the value
-        value++;
-
-        // Attempt to store the incremented value back to *ptr
-        asm volatile ("strex %0, %2, [%1]"
-                      : "=&r" (success)
-                      : "r" (ptr), "r" (value)
-                      : "cc", "memory"); // Use "cc" and "memory" clobbers
-    } while (success != 0); // Repeat if the store was not successful
 }
 
 /**
