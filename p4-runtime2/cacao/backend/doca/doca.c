f3b18f702c0092d60af3b46ec5b9948d70202ee4
yihan <yangyihan1998@gmail.com>
2024-10-15 13:02:29 +0000

    update

--- p4-runtime2/cacao/backend/doca/doca.c
+++ p4-runtime2/cacao/backend/doca/doca.c
@@ -881,6 +881,20 @@ int doca_create_hw_pipe_for_port(struct doca_flow_pipe **pipe, struct flow_pipe_
 		return result;
 	}
 
+	if (doca_fwd.type != DOCA_FLOW_FWD_CHANGEABLE) {
+		result = doca_flow_pipe_add_entry(0, doca_pipe, &doca_match, &doca_actions, NULL, doca_fwd_ptr, 0, &status, &entry);
+		if (result != DOCA_SUCCESS) {
+			printf(ESC LIGHT_RED "[ERR]" RESET " Failed to add entry to pipe on port %d (%s)\n", port_id, doca_get_error_string(result));
+			return result;
+		}
+
+		result = doca_flow_entries_process(ports[port_id], 0, PULL_TIME_OUT, num_of_entries);
+		if (result != DOCA_SUCCESS) {
+			printf(ESC LIGHT_RED "[ERR]" RESET " Failed to process entry to pipe on port %d (%s)\n", port_id, doca_get_error_string(result));
+			return result;
+		}
+	}
+
 	printf(ESC GREEN "[INFO]" RESET " Created pipe %s on port %d\n", pipe_cfg->attr.name, port_id);
 #endif
 	*pipe = doca_pipe;
